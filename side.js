549236f36a69a953b95b1469045b1a59dc78ef14
David <te16.david.andersson@umea.nti.se>
2018-12-07 09:44:20 +0100

    collision fungerar i x-led för alla spawn punkter

--- side.js
+++ side.js
@@ -2,8 +2,8 @@ let canvas = document.getElementById("spel");
 let ctx = canvas.getContext("2d");
 let firstimgX = 0;
 let secondimgX = 0;
-firstStartPointX = 355;
-secondStartPointX = 870;
+firstStartPointX = Math.floor((Math.random()*899) + 1);
+secondStartPointX = Math.floor((Math.random()*899) + 1);
 if (firstStartPointX > secondStartPointX) {
 	firstStartPointDiffX = firstStartPointX - secondStartPointX;
 } else if (firstStartPointX < secondStartPointX) {
@@ -12,6 +12,8 @@ if (firstStartPointX > secondStartPointX) {
 	firstStartPointDiffX = 0;
 }
 firstStartPointDiffHalfX = firstStartPointDiffX / 2;
+firstStartIsIntegerX = firstStartPointX / 2;
+secondStartIsIntegerX = secondStartPointX / 2;
 
 
 let i = 0;
@@ -25,6 +27,7 @@ function draw() {
 	ctx.drawImage(fgImg, secondimgX, 0, 0, 20);
 	ctx.drawImage(fgImg, (secondimgX + secondStartPointX), 0, 20, 20);
 
+if (Number.isInteger(firstStartIsIntegerX) && Number.isInteger(secondStartIsIntegerX)) {
 if (firstStartPointX > secondStartPointX) {
 	if (firstimgX + firstStartPointDiffX != secondimgX) {
 		if (firstimgX <= secondStartPointX) {
@@ -33,11 +36,11 @@ if (firstStartPointX > secondStartPointX) {
 			secondimgX = secondimgX + 1;
 		}
 		console.log(firstimgX + +firstStartPointDiffX + " " + secondimgX);
-		} else if (firstimgX + firstStartPointDiffX  == secondimgX) {
-			console.log("TRUUUUUE");
-			console.log(firstStartPointDiffX);
-			ctx.clearRect(0, 0, 900, 900);
-		}
+	} else if (firstimgX + firstStartPointDiffX  == secondimgX) {
+		console.log("TRUUUUUE");
+		console.log(firstStartPointDiffX);
+		ctx.clearRect(0, 0, 900, 900);
+	}
 } else {
 	if (secondimgX + firstStartPointDiffX != firstimgX) {
 		if (secondimgX <= firstStartPointX) {
@@ -47,17 +50,116 @@ if (firstStartPointX > secondStartPointX) {
 		firstimgX = firstimgX + 1;
 		}
 		console.log(secondimgX + +firstStartPointDiffX + " " + firstimgX);
-		} else if (secondimgX + firstStartPointDiffX == firstimgX) { //TODO gör så att om ena börjar på ett ojämt nummer och de andra på ett jämt att de fortfarande detectar, ändra == till <= eller nåt...
-			console.log("TRUUUUUE");
-			console.log(firstStartPointDiffX);
-			ctx.clearRect(0, 0, 900, 900);
+	} else if (secondimgX + firstStartPointDiffX == firstimgX) { //TODO gör så att om ena börjar på ett ojämt nummer och de andra på ett jämt att de fortfarande detectar, ändra == till <= eller nåt...
+		console.log("TRUUUUUE");
+		console.log(firstStartPointDiffX);
+		ctx.clearRect(0, 0, 900, 900);
+	}
+}
+
+	i++;
+	if ( i < 900)
+		window.requestAnimationFrame(draw);
+} else if (Number.isFinite(firstStartIsIntegerX) && Number.isInteger(secondStartIsIntegerX)) {
+if (firstStartPointX > secondStartPointX) {
+	if (firstimgX + firstStartPointDiffX + 1 != secondimgX) {
+		if (firstimgX <= secondStartPointX) {
+			firstimgX = firstimgX - 1;
+		} if (secondimgX <= firstStartPointX) {
+			secondimgX = secondimgX + 1;
 		}
+		console.log(firstimgX + +firstStartPointDiffX + 1 + " " + secondimgX);
+	} else if (firstimgX + firstStartPointDiffX + 1 == secondimgX) {
+		console.log("first+1");
+		console.log(firstStartPointDiffX);
+		ctx.clearRect(0, 0, 900, 900);
+	}
+} else {
+	if (secondimgX + firstStartPointDiffX + 1 != firstimgX) {
+		if (secondimgX <= firstStartPointX) {
+		secondimgX = secondimgX - 1;
+		}
+		if (firstimgX <= secondStartPointX) {
+		firstimgX = firstimgX + 1;
+		}
+		console.log(secondimgX + +firstStartPointDiffX + 1 + " " + firstimgX);
+	} else if (secondimgX + firstStartPointDiffX + 1 == firstimgX) {
+		console.log("TRUUUUUE");
+		console.log(firstStartPointDiffX);
+		ctx.clearRect(0, 0, 900, 900);
+	}
+}
+
+	i++;
+	if ( i < 900)
+		window.requestAnimationFrame(draw);
+}else if (Number.isInteger(firstStartIsIntegerX) && Number.isFinite(secondStartIsIntegerX)) {
+if (firstStartPointX > secondStartPointX) {
+	if (firstimgX + firstStartPointDiffX + 1 != secondimgX) {
+		if (firstimgX <= secondStartPointX) {
+			firstimgX = firstimgX - 1;
+		} if (secondimgX <= firstStartPointX) {
+			secondimgX = secondimgX + 1;
+		}
+		console.log(firstimgX + +firstStartPointDiffX + 1 + " " + secondimgX);
+	} else if (firstimgX + firstStartPointDiffX + 1  == secondimgX) {
+		console.log("TRUUUUUE");
+		console.log(firstStartPointDiffX);
+		ctx.clearRect(0, 0, 900, 900);
+	}
+} else {
+	if (secondimgX + firstStartPointDiffX + 1 != firstimgX) {
+		if (secondimgX <= firstStartPointX) {
+		secondimgX = secondimgX - 1;
+		}
+		if (firstimgX <= secondStartPointX) {
+		firstimgX = firstimgX + 1;
+		}
+		console.log(secondimgX + +firstStartPointDiffX + 1 + " " + firstimgX);
+	} else if (secondimgX + firstStartPointDiffX + 1 == firstimgX) { //TODO gör så att om ena börjar på ett ojämt nummer och de andra på ett jämt att de fortfarande detectar, ändra == till <= eller nåt...
+		console.log("TRUUUUUE");
+		console.log(firstStartPointDiffX);
+		ctx.clearRect(0, 0, 900, 900);
+	}
+}
+
+	i++;
+	if ( i < 900)
+		window.requestAnimationFrame(draw);
+}else if (Number.isFinite(firstStartIsIntegerX) && Number.isFinite(secondStartIsIntegerX)) {
+if (firstStartPointX > secondStartPointX) {
+	if (firstimgX + firstStartPointDiffX != secondimgX) {
+		if (firstimgX <= secondStartPointX) {
+			firstimgX = firstimgX - 1;
+		} if (secondimgX <= firstStartPointX) {
+			secondimgX = secondimgX + 1;
+		}
+		console.log(firstimgX + +firstStartPointDiffX + " " + secondimgX);
+	} else if (firstimgX + firstStartPointDiffX  == secondimgX) {
+		console.log("TRUUUUUE");
+		console.log(firstStartPointDiffX);
+		ctx.clearRect(0, 0, 900, 900);
+	}
+} else {
+	if (secondimgX + firstStartPointDiffX != firstimgX) {
+		if (secondimgX <= firstStartPointX) {
+		secondimgX = secondimgX - 1;
+		}
+		if (firstimgX <= secondStartPointX) {
+		firstimgX = firstimgX + 1;
+		}
+		console.log(secondimgX + +firstStartPointDiffX + " " + firstimgX);
+	} else if (secondimgX + firstStartPointDiffX == firstimgX) { //TODO gör så att om ena börjar på ett ojämt nummer och de andra på ett jämt att de fortfarande detectar, ändra == till <= eller nåt...
+		console.log("TRUUUUUE");
+		console.log(firstStartPointDiffX);
+		ctx.clearRect(0, 0, 900, 900);
+	}	
 }
 
 	i++;
 	if ( i < 900)
 		window.requestAnimationFrame(draw);
 }
-
+}
 
 window.requestAnimationFrame(draw);
